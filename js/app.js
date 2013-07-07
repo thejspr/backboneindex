08e7d84772bec9407f3bb3f407f083bb18979490
Jesper Kjeldgaard <jkjeldgaard@housetrip.com>
2013-07-07 23:41:34 +0100

    Basic  loading with backbone done

--- js/app.js
+++ js/app.js
@@ -1,25 +1,33 @@
-var Extension = Backbone.Model.extend();
+$(function(){
+  var Extension = Backbone.Model.extend();
 
-var ExtensionList = Backbone.Collection.extend({
-  model: Extension,
-  url: 'data/extensions.json'
+  var ExtensionList = Backbone.Collection.extend({
+    model: Extension,
+    url: 'data/extensions.json'
+  });
+
+  var ExtensionView = Backbone.View.extend({
+    el: "#extensions",
+    template: _.template("<div class='extension'><h3><%= title %></h3><p><%= description %></p></div>"),
+    render: function(eventName) {
+      _.each(this.model.models, function(extension){
+        var extensionTemplate = this.template(extension.attributes);
+        $(this.el).append(extensionTemplate);
+      }, this);
+
+      return this;
+    }
+  });
+
+  var extensions = new ExtensionList();
+
+  extensions.fetch({
+    success: function(){
+      var extensionsView = new ExtensionView({model: extensions});
+      extensionsView.render();
+    },
+    error: function() {
+      console.log('An error occurred loading the json')
+    }
+  });
 });
-
-var ExtensionView = Backbone.View.extend({
-  el: "#extensions",
-  template: _.template("<div class='extension'><h3><%= title %></h3><p><%= description %></p></div>"),
-  render: function(eventName) {
-    _.each(this.model.models, function(extension){
-      var extensionTemplate = this.template(extension);
-      $(this.el).append(extensionTemplate);
-    }, this);
-
-    return this;
-  }
-});
-
-var extensions = new ExtensionList();
-extensions.fetch();
-
-var extensionsView = new ExtensionView({model: extensions});
-extensionsView.render();
