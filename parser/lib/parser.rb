0aebc171e53bbb65e03d1316a174cddc681371e5
Jesper Kjeldgaard <jkjeldgaard@housetrip.com>
2013-07-07 23:59:34 +0100

    restore newlines in descriptions

--- parser/lib/parser.rb
+++ parser/lib/parser.rb
@@ -22,7 +22,7 @@ class Parser
       next if lines.empty?
       { title: lines.first,
         category: category,
-        description: lines[1..-1].join(' ').strip }
+        description: lines[1..-1].join("\n").strip }
     end.compact
   end
 
