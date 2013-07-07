bddffae7ddf79ca2899cb953e60a335547de95c0
Jesper Kjeldgaard <jkjeldgaard@housetrip.com>
2013-07-07 20:12:59 +0100

    Add scripts for building and testing

--- lib/app.rb
+++ lib/app.rb
@@ -2,10 +2,8 @@ require './lib/parser'
 require 'json'
 
 class App
-  def self.parse(filename = 'Extensions,-Plugins,-Resources.md')
+  def self.build_json(filename = 'Extensions,-Plugins,-Resources.md')
     result = Parser.parse("backbone.wiki/#{filename}")
     File.open("data/#{filename}.json", 'w') { |f| f.write(result.to_json) }
   end
 end
-
-App.parse
