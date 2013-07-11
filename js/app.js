d979b46364909c53c2fd77116a0823ef8eae3605
Jesper Kjeldgaard <jkjeldgaard@housetrip.com>
2013-07-11 21:37:13 +0100

    Filtering WIP

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
-$(function(){
-  var extensions = new ExtensionList();
+var AppState  = Backbone.Model.extend({});
+var appState = new AppState;
 
-  function init() {
-    var extensionsView = new ExtensionView({model: extensions});
-    extensionsView.render();
-  }
+var categories = new CategoryList();
+var extensions = new ExtensionList();
 
-  extensions.fetch({
-    success: init
-  });
-});
+appState.set('categories', categories);
+appState.set('extensions', extensions);
+
+function initCategories() {
+  var categoriesView = new CategoryView({model: categories});
+  categoriesView.render();
+}
+
+function initExtensions() {
+  var extensionsView = new ExtensionView({model: extensions});
+  extensionsView.render();
+}
+
+categories.fetch({ success: initCategories });
+extensions.fetch({ success: initExtensions });
--- js/app/category.js
+++ js/app/category.js
@@ -6,14 +6,28 @@ var CategoryList = Backbone.Collection.extend({
 });
 
 var CategoryView = Backbone.View.extend({
+  events: {
+    'click .category': 'updateList'
+  },
   el: "#categories",
-  template: _.template("<label class='checkbox'><input type='checkbox' value='<%= title %>' checked='checked'><%= title %></label>"),
+  template: _.template("<label class='checkbox'><input class='category' type='checkbox' value='<%= title %>' checked='checked'><%= title %></label>"),
   render: function() {
     _.each(this.model.models, function(category){
-      var extensionTemplate = this.template(extension.toAttributes());
-      $(this.el).append(extensionTemplate);
+      var categoryTemplate = this.template(category.attributes);
+      $(this.el).append(categoryTemplate);
     }, this);
 
     return this;
+  },
+  updateList: function(){
+    var categories = [];
+
+    $('.category:checked').each(function(){
+      categories.push($(this).val());
+    });
+
+    var extensions = appState.get('extensions').models;
+    var result = queryEngine.createCollection(extensions).findAll({category: {$in: categories}});
+    new ExtensionView({model: result}).render();
   }
 });
